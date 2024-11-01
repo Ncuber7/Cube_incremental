1d9aabd46cb7dc1aa5e7ff5af15d2c388f2a5fb0
Ncuber7 <tomatoumauma231@gmail.com>
2024-11-02 07:16:39 +0900

    click

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log("app.js loaded!");
+let clicks = 0
+document.getElementById("text-button").onclick = function() {
+  clicks++
+    document.getElementById("clickstime").innerHTML = clicks;
+  };
